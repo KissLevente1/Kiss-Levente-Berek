29f2cc869af462bb1a7b559751726769b2d9d734
KissLevente1 <113094877+KissLevente1@users.noreply.github.com>
2024-11-19 09:30:33 +0100

    Add project files.

---
+++ Kiss Levente Berek/Dolgozo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiss_Levente_Berek
+{
+    internal class Dolgozo
+    {
+        public string Nev { get; set; }
+        public string Nem { get; set; }
+        public string Reszleg { get; set;}
+        public int BelepesEv { get; set; }
+        public decimal Ber { get; set;}
+
+        public Dolgozo(string s)
+        {
+            var v = s.Split(';');
+            Nev = v[0];
+            Nem = v[1];
+            Reszleg = v[2];
+            BelepesEv = int.Parse(v[3]);
+            Ber = decimal.Parse(v[4]);
+        }
+    }
+}
---
+++ Kiss Levente Berek/Program.cs
@@ -0,0 +1,33 @@
+﻿using Kiss_Levente_Berek;
+
+//27 perc
+
+List<Dolgozo> dolgozok = new List<Dolgozo>();
+StreamReader sr = new StreamReader(@"..\..\..\src\berek2020.txt");
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    dolgozok.Add(new Dolgozo(sr.ReadLine()));
+}
+sr.Close();
+
+Console.WriteLine("3. feladat:");
+Console.WriteLine($"{dolgozok.Count} dolgozó adata szerepel a forrásban.");
+
+Console.WriteLine("\n4. feladat: ");
+var atlagBer2020 = dolgozok.Average(d => d.Ber);
+Console.WriteLine($"2020-as átlagbér: {Math.Round(atlagBer2020/1000,1)}");
+
+Console.WriteLine("\n5. feladat:");
+Console.Write("Részleg neve: ");
+string reszlegInput = Console.ReadLine();
+var legtobbFizetes = dolgozok.OrderByDescending(d => d.Ber).First();
+Console.WriteLine($"Legnagyobb bérrel remdelkező dolgozó adatai: {legtobbFizetes.Nev} {legtobbFizetes.Nem} {legtobbFizetes.Reszleg} {legtobbFizetes.BelepesEv} {legtobbFizetes.Ber}");
+
+Console.WriteLine("\n6. feladat: ");
+var reszlegek = dolgozok.GroupBy(d=>d.Reszleg).Select(g => new { Reszleg = g.Key, DolgozoSzam = g.Count() }).ToList();
+Console.WriteLine("Statisztika: ");
+foreach (var item in reszlegek)
+{
+    Console.WriteLine($"{item.Reszleg} {item.DolgozoSzam}");
+}
